61c564a18388c42852f3f563a5462d9cc84b0a94
Kevin Olinger <mail@kevyn.lu>
2018-06-13 23:32:14 +0200

    Added abstract processor for custom Socket.IO events

---
+++ src/main/java/lu/kevyn/dw_core/event/Event.java
@@ -0,0 +1,86 @@
+/**
+ * 
+ * BSD 3-Clause License
+ * 
+ * Copyright (c) 2018+ Kevin Olinger
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *    
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *    
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *    
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ */
+
+package lu.kevyn.dw_core.event;
+
+import com.corundumstudio.socketio.AckRequest;
+import com.corundumstudio.socketio.SocketIOClient;
+import com.corundumstudio.socketio.listener.DataListener;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import lu.kevyn.dw_core.Core;
+
+public abstract class Event implements DataListener<JsonNode> {
+	
+	Core DW;
+	
+	SocketIOClient client;
+	ObjectMapper mapper;
+	JsonNode data;
+	
+	public Event(Core DW) {
+		this.DW = DW;
+	}
+	
+	@Override
+	public void onData(SocketIOClient client, JsonNode data, AckRequest ackSender) throws Exception {
+		this.client = client;
+		this.data = data;
+		
+		DW.log.socket.in(data);
+		
+		execute();
+	}
+	
+	public abstract void execute();
+	
+	public void sendError(String errCode, String message) {
+		if(mapper == null) mapper = new ObjectMapper();
+		
+		ObjectNode obj = mapper.createObjectNode();
+		obj.put("errCode", errCode);
+		obj.put("errMsg", message);
+		obj.put("given_data", data.toString());
+		
+		if(data.has("from")) obj.put("to", data.get("from").textValue());
+		
+		DW.log.socket.out(obj);
+		
+		client.sendEvent("error", obj);
+	}
+
+}
